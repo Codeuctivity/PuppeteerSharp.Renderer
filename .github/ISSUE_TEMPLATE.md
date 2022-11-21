527cfc3d01bab1ad55d98dcbe20ac196e6d4f290
Stefan Seeland <168659+stesee@users.noreply.github.com>
2022-11-21 19:26:43 +0100

    Added trimming speicifc integration test

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,19 @@
+## Expected Behavior
+
+
+## Actual Behavior
+
+
+## Steps to Reproduce the Problem
+
+  1.
+  1.
+  1.
+
+## Specifications
+
+  - Version:
+  - OS:
+  - CPU Architecture:
+  - .net Version:
+  - .net Trimming:
--- Codeuctivity.HtmlRendererCliTests/RendererCliTests.cs
+++ Codeuctivity.HtmlRendererCliTests/RendererCliTests.cs
@@ -1,5 +1,6 @@
 using Codeuctivity.HtmlRendererCli;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Xunit;
@@ -25,6 +26,89 @@ namespace Codeuctivity.HtmlRendererCliTests
             await Program.Main(new[] { sourceHtmlFilePath, actualFilePath });
 
             Assert.True(File.Exists(actualFilePath));
+        }
+
+        [FactRunableOnWindows]
+        public void PublishedSelfContainedBinaryShouldWork()
+        {
+            var testFileName = "BasicTextFormated.html";
+            var sourceHtmlFilePath = Path.GetFullPath($"../../../TestInput/{testFileName}");
+            var actualFilePath = Path.Combine(Path.GetTempPath(), $"Actual{testFileName}.pdf");
+
+            if (File.Exists(actualFilePath))
+            {
+                File.Delete(actualFilePath);
+            }
+
+            var acutualWindowsBinary = DotnetPublishFolderProfileWindows("Codeuctivity.HtmlRendererCli");
+
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = acutualWindowsBinary,
+                    Arguments = $" {sourceHtmlFilePath} {actualFilePath}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            var isExited = process.WaitForExit(60000);
+
+            if (!isExited)
+            {
+                process.Kill();
+                // run on windows: Stop-Process -Name "chrome"
+            }
+
+            Assert.True(isExited);
+
+            Assert.True(File.Exists(actualFilePath));
+        }
+
+        private static string DotnetPublishFolderProfileWindows(string projectName)
+        {
+            var absolutePath = Path.GetFullPath("../../../../" + projectName);
+            var expectedBinaryPath = Path.Combine(absolutePath, $"bin/Release/net7.0/publish/FolderProfileWindows/{projectName}.exe");
+
+            if (File.Exists(expectedBinaryPath))
+            {
+                File.Delete(expectedBinaryPath);
+            }
+
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = $"publish {absolutePath} --configuration Release -f net7.0 -p:PublishProfile=FolderProfileWindows",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+
+            var outputResult = GetStreamOutput(process.StandardOutput);
+            var errorResult = GetStreamOutput(process.StandardError);
+            Assert.True(process.WaitForExit(20000));
+
+            Assert.Empty(errorResult);
+
+            Assert.True(File.Exists(expectedBinaryPath), outputResult);
+
+            return expectedBinaryPath;
+        }
+
+        private static string GetStreamOutput(StreamReader stream)
+        {
+            var outputReadTask = Task.Run(() => stream.ReadToEnd());
+
+            return outputReadTask.Result;
         }
     }
 }
